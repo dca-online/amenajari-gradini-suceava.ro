667682939fcf30439532a5ac24452c48b87edb03
dca-online <dca.webdesigns@gmail.com>
2024-11-27 19:24:07 +0200

    Security

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 const firebaseConfig = {
-        apiKey: "AIzaSyAjBiPfqc5yG0dgD9VSXS61_seismsMCPo",
-        authDomain: "proiectbeutesting.firebaseapp.com",
-        databaseURL: "https://proiectbeutesting-default-rtdb.europe-west1.firebasedatabase.app",
-        projectId: "proiectbeutesting",
-        storageBucket: "proiectbeutesting.firebasestorage.app",
-        messagingSenderId: "662539129588",
-        appId: "1:662539129588:web:1b69ead30fb21494fec4a1",
-        measurementId: "G-PW83LJGYLP"
+        apiKey: process.env.FIREBASE_API_KEY,
+        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+        databaseURL: process.env.FIREBASE_DATABASE_URL,
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+        appId: process.env.FIREBASE_APP_ID,
+        measurementId: process.env.FIREBASE_APP_ID
 };
 
 if (!firebase.apps.length) {
