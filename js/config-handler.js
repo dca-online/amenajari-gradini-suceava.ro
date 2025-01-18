6b618f2e2010524da432a046b0c952dd0b3b7bd4
dca-online <dca.webdesigns@gmail.com>
2025-01-18 10:44:58 +0200

    Update 18 Jan

--- js/config-handler.js
+++ js/config-handler.js
@@ -1,40 +1,38 @@
-// function loadFirebaseConfig() {
-//     return new Promise((resolve) => {
-//         const script = document.createElement('script');
-//         script.src = window.location.hostname === 'localhost' 
-//             ? 'js/firebase-init.local.js' 
-//             : 'js/firebase-init.prod.js';
-//         script.onload = () => {
-//             // Initialize WOW and other UI components
-//             new WOW().init();
+function loadFirebaseConfig() {
+    return new Promise((resolve) => {
+        const script = document.createElement('script');
+        script.src = window.location.hostname === 'localhost' 
+            ? 'js/firebase-init.local.js' 
+            : 'js/firebase-init.prod.js';
+        script.onload = () => {
+            // Initialize WOW and other UI components
+            new WOW().init();
             
-//             // Initialize carousel
-//             $(".header-carousel").owlCarousel({
-//                 autoplay: true,
-//                 smartSpeed: 1500,
-//                 items: 1,
-//                 dots: true,
-//                 loop: true,
-//                 nav: true,
-//                 navText: [
-//                     '<i class="bi bi-chevron-left"></i>',
-//                     '<i class="bi bi-chevron-right"></i>'
-//                 ]
-//             });
+            $(".header-carousel").owlCarousel({
+                autoplay: true,
+                smartSpeed: 1500,
+                items: 1,
+                dots: true,
+                loop: true,
+                nav: true,
+                navText: [
+                    '<i class="bi bi-chevron-left"></i>',
+                    '<i class="bi bi-chevron-right"></i>'
+                ]
+            });
             
-//             // Remove spinner
-//             const spinner = document.getElementById('spinner');
-//             if (spinner) {
-//                 spinner.classList.remove('show');
-//             }
-//             resolve();
-//         };
-//         document.head.appendChild(script);
-//     });
-// }
+            const spinner = document.getElementById('spinner');
+            if (spinner) {
+                spinner.classList.remove('show');
+            }
+            resolve();
+        };
+        document.head.appendChild(script);
+    });
+}
 
-// loadFirebaseConfig().then(() => {
-//     const mainScript = document.createElement('script');
-//     mainScript.src = 'js/main.js';
-//     document.head.appendChild(mainScript);
-// });
+loadFirebaseConfig().then(() => {
+    const mainScript = document.createElement('script');
+    mainScript.src = 'js/main.js';
+    document.head.appendChild(mainScript);
+});
